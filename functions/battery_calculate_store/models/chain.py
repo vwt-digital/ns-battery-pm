2c1ffe05fe1a58bc99493f8e3dca143751898028
MaxvanderWeide <max.vanderweide@student.hu.nl>
2021-05-10 16:44:02 +0200

    Added chain class DAT-6450

--- functions/battery_calculate_store/models/chain.py
+++ functions/battery_calculate_store/models/chain.py
@@ -1,3 +1,17 @@
+from typing import Any, List
+
+from models.chain_instance import ChainInstance
+
+
 class Chain(object):
-    def __init__(self):
-        pass
+    def __init__(self, collected: Any, chains: List[ChainInstance]):
+        self.collected = collected
+        self.chains = chains
+
+    @staticmethod
+    def from_dict(source) -> object:
+        return Chain(source["collected"], source["chains"])
+
+    def add_chain_instance(self, chain_instance: ChainInstance) -> object:
+        self.chains.append(chain_instance)
+        return self
