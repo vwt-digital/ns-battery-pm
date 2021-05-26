f91ed67637154f0bd760417b6c65d8b8d1a3ea1d
MaxvanderWeide <max.vanderweide@student.hu.nl>
2021-05-26 23:18:52 +0200

    First decision version based on calculated growth DAT-6549

--- functions/battery_decision_tree/utils/handle_decision.py
+++ functions/battery_decision_tree/utils/handle_decision.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from google.cloud import firestore
 from models.enum import Decision
 
@@ -7,9 +5,9 @@ from models.enum import Decision
 class HandleDecision:
     def __init__(self):
         self.db = firestore.Client()
-        self.deciding_growth_limit = 0.09
+        self.deciding_growth_limit = 0.088
 
-    def _retrieve_calculated(self, battery_name: str) -> Any:
+    def _retrieve_calculated(self, battery_name: str):
         return (
             self.db.collection("battery_actual")
             .document("calculated")
@@ -21,30 +19,28 @@ class HandleDecision:
     def _weekly_with_limit(
         self, average_growth_change_rate, last_stored, limit: int = 10
     ):
-        print(average_growth_change_rate, last_stored)
         if (
             last_stored - (average_growth_change_rate * limit)
             > self.deciding_growth_limit
         ):
-            return Decision.SAFE.format(weeks=limit)
+            return Decision.SAFE.value.format(weeks=limit)
 
-        for x in reversed(range(limit - 1)):
+        for x in range(limit):
             if (
-                last_stored - (average_growth_change_rate * x + 1)
+                last_stored - (average_growth_change_rate * (x + 1))
                 <= self.deciding_growth_limit
             ):
-                return Decision.UNSAFE.format(x + 1)
-        return Decision.UNDETERMINED
+                return Decision.UNSAFE.value.format(weeks=x + 1)
+        return Decision.UNDETERMINED.value
 
     def decide(self, battery_name: str) -> object:
         calculated_stored = self._retrieve_calculated(battery_name).stream()
-
         arr = []
-        last_stored = 0
+        last_stored = None
 
-        for doc in calculated_stored.stream():
+        for doc in calculated_stored:
             dicted = doc.to_dict()
-            if arr:
+            if not (last_stored is None):
                 arr.append(float(last_stored - dicted["growth"]))
             last_stored = float(dicted["growth"])
 
